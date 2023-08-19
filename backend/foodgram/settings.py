69b0a8b617f575d44a56925f6138daa53926fc49
Вячеслав Бухтояров <fklskadelovoy@gmail.com>
2023-08-19 17:44:07 +1000

    Please work

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['158.160.3.99', '127.0.0.1', 'localhost', 'foodgram-fklska.hopto.org', 'https://foodgram-fklska.hopto.org']
+ALLOWED_HOSTS = ['foodgram-fklska.hopto.org']
 
 # Application definition
 
@@ -70,9 +70,9 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
