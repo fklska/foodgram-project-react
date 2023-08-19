88a3a8288dec1223a4556acc1573dc2518ed4282
Вячеслав Бухтояров <fklskadelovoy@gmail.com>
2023-08-19 17:23:47 +1000

    add allowed host

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['158.160.3.99', '127.0.0.1', 'localhost', 'https://foodgram-fklska.hopto.org/']
 
 # Application definition
 
