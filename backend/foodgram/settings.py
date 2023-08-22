f1476a6914e7f9ff9c94558bf437b471570a063d
Вячеслав Бухтояров <fklskadelovoy@gmail.com>
2023-08-22 11:42:28 +1000

    Change page number

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -45,8 +45,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",
     ],
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
-    "PAGE_SIZE": 7,
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination"
 }
 
 DJOSER = {
