c154fd2bb8a2d54aaa11f979da5805729fbe7a19
Вячеслав Бухтояров <fklskadelovoy@gmail.com>
2023-08-27 08:26:36 +1000

    Add pub date

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -15,6 +15,7 @@ class RecipeAdmin(admin.ModelAdmin):
         "amount_in_favorite",
         "pub_date"
     )
+    list_editable = ("pub_date", "name",)
     list_filter = (
         "name",
         "author",
