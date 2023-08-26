b4f8070a9e163ff4be01f7947c4d551f367343d8
Вячеслав Бухтояров <fklskadelovoy@gmail.com>
2023-08-27 08:13:44 +1000

    Add pub date

--- backend/api/models.py
+++ backend/api/models.py
@@ -54,7 +54,9 @@ class Recipe(models.Model):
         verbose_name="Время приготовления"
     )
     pub_date = models.DateTimeField(
-        auto_now_add=True
+        auto_now_add=True,
+        blank=True,
+        null=True
     )
 
     class Meta:
