40964232522114a7556c59483736a0da4efc6b03
Вячеслав Бухтояров <fklskadelovoy@gmail.com>
2023-08-26 20:41:39 +1000

    Try to add constrain on follow

--- backend/users/models.py
+++ backend/users/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.core.exceptions import ValidationError
 
+
 class User(AbstractUser):
     """Модель пользователя."""
 
